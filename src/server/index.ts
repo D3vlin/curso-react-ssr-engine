b2e0bcc5a5b02ae583f59da58b847cae3238d854
D3vlin <alexisdq16@gmail.com>
2025-03-02 09:16:57 -0500

    server with express

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,1 +1,13 @@
-console.log("server");
+import express, {Express, Request, Response} from 'express'
+import { config } from './config'
+
+const app: Express = express()
+
+app.get('*', (req: Request, res: Response) => {
+    res.send(`<h1>Path: ${req.url}<h1>`)
+})
+
+app.listen(config.PORT, () => {
+    console.log(`Listening in http://localhost:${config.PORT}`);
+    
+})
